2a1a3587348de0cb97278ab6a1bd274e12932eed
Ray Ortiz <oray@bgsu.edu>
2023-04-04 08:15:02 -0400

    week1 challenges + test file

---
+++ Week1/Week1.js
@@ -0,0 +1,2 @@
+// Your goal on this stage is to simply give the variable a some number value.
+const x = 13
---
+++ Week1/test.js
@@ -0,0 +1,7 @@
+const assert = require("assert");
+const x = require("../Week1/Week1");
+
+it("should store a number", () => {
+    const result = x.storeNumber(10);
+  assert.equal(typeof assert, "number");
+});
