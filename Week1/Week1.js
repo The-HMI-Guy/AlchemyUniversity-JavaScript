282597341ed8f1b4e016bed5a7d56f57d6d24098
Ray Ortiz <oray@bgsu.edu>
2023-04-05 09:28:01 -0400

    Test and Week updated.

--- Week1/Week1.js
+++ Week1/Week1.js
@@ -1,4 +1,20 @@
+// Intro To JavaScript - Number Variable #1
 // Your goal on this stage is to simply give the variable a some number value.
 const x = 13;
-
-module.exports = x;
+// Intro To JavaScript - Multiple Variables #2
+// Create two variables a and b with the same value in each.
+const a = 13;
+const b = a;
+// Intro To JavaScript - Booleans #3
+// Create two boolean variables c and d. Store true in one and false in the other.
+const c = true;
+const d = false;
+// Intro To JavaScript - Strings #4
+// In the variable e is stored the value "World". Your task is to store "Hello World" in the variable f.
+const e = "World";
+const f = `Hello ${e}`;
+// Intro To JavaScript - Mutable Let #5
+// Change the code to use the keyword let instead of const so the value can be re-assigned.
+let g = 5;
+g = 7;
+module.exports = { x, a, b, c, d, e, f,g };
--- Week1/test.js
+++ Week1/test.js
@@ -1,7 +1,24 @@
 const assert = require("assert");
-const x = require("../Week1/Week1");
+const { x, a, b, c, d, f, g } = require("../Week1/Week1");
 
 it("should store a number", () => {
-    //const result = x.storeNumber(10);
+  //const result = x.storeNumber(10);
   assert.equal(typeof x, "number");
+});
+
+it("should check if a is equal to b", () => {
+  assert.equal(a, b);
+});
+
+it("should check for c to be true and d to be false", () => {
+  assert.equal(c, true);
+  assert.equal(d, false);
+});
+
+it("should check that e equals Hello World", () => {
+  assert.equal(f, "Hello World");
+});
+
+it("should check if g is equal to 7", () => {
+  assert.equal(g, 7);
 });
